0abea01bc0255dba624b6d1e146ff2f5297de397
reathh <Ivelin Kirilov>
2015-01-04 20:29:19 +0200

    Implemented the navigation panel

--- js/controllers/NavigationController.js
+++ js/controllers/NavigationController.js
@@ -1,3 +1,15 @@
 adsApp.controller('NavigationController', function NavigationController($scope) {
+    $scope.navItems = [
+        {
+            text: "Home",
+            url: "#/view/ads",
+            active: "active"
+        },
 
+        {
+            text: "Home",
+            url: "#/view/ads",
+            active: ""
+        }
+    ]
 });
---
+++ js/directives/navigationItemDirective.js
@@ -0,0 +1,7 @@
+adsApp.directive('navigationItemDirective', function() {
+    return {
+        restrict: 'AE',
+        templateUrl: 'templates/directives/navigation-item-directive.html',
+        replace: true
+    }
+});
