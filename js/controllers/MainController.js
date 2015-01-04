7d83f2de6eccc21c3cbfd31fb6bf5a6fdcd6d415
reathh <Ivelin Kirilov>
2015-01-04 19:22:03 +0200

    Please Login box completed

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,5 @@
 adsApp.controller('MainController', function MainController($scope) {
     $scope.currentPageString = "Home";
     $scope.showPleaseLoginPanel = true;
+    $scope.pleaseLoginPanel = 'templates/general/please-login-panel.html';
 });
